b95a0f14e7b0fa471ec86ea65e0ea1742e6c0c62
Jarrett Milner <milnerj@purdue.edu>
2013-04-23 15:27:58 -0400

    Worked on trait

--- Generations/src/com/generations/Trait.java
+++ Generations/src/com/generations/Trait.java
@@ -5,6 +5,14 @@ public class Trait {
 	private String name;
 	private String trait;
 	
+	private String[] traits = {"hair color", "eye olor", "widows peak", "connected earlobes", "cleft chin"};
+	
+	private String[] hairColor = {"brown", "blonde", "red"};
+	private String[] eyeColor = {"brown", "hazel", "green", "blue"};
+	private String[] widowsPeak = {"yes", "no"};
+	private String[] connectedEarlobes = {"no", "yes"};
+	private String[] cleftChin = {"yes", "no"};
+	
 	/**
 	 * Constructor
 	 * 
@@ -33,5 +41,57 @@ public class Trait {
 	}
 	
 	//TODO: Jarrett You can use this area for trait tracking as well
+	
+	public Person createChild(String name, boolean male, Person p1, Person p2){
+		Person child = new Person(name, male, p1, p2);
+		
+		//gets the alleles from parent 1 and parent 2
+		Person gp1p1 = p1.getParent(0);
+		Person gp2p1 = p1.getParent(1); 
+		
+		
+		Person gp1p2 = p2.getParent(0);
+		Person gp2p2 = p2.getParent(1);
 
+//		combineTraits()
+		
+		return child;
+	}
+	
+	public Person getAlleles(Person p1, Person p2, Person p3){
+		if(traits[0].equals("hair color")){
+			
+		} else if(traits[1].equals("eye color")){
+			
+		} else if(traits[2].equals("widows peak")){
+			if(p2.getTrait("widows peak") == null || p3.getTrait("widows peak") == null){
+				if(p1.getTrait("widows peak").getTrait() == widowsPeak[0]){
+					//alleles are 2 dominant
+				} else{
+					//alleles are 2 recessive
+				}
+			}
+			
+		}
+		return p1;
+	}
+	
+	public Trait combineTraits(Trait t1, Trait t2){
+		if(t1.getName().equals(t2.getName())){
+			System.out.println("cannot compare");
+			return null;
+		}
+		if(t1.getName().equals("hair color")){
+			
+		} else if(t1.getName().equals("eye color")){
+			
+		} else if(t1.getName().equals("widows peak")){
+
+		} else if(t1.getName().equals("connected earlobes")){
+			
+		} else if(t1.getName().equals("cleft chin")){
+			
+		}
+		return null;
+	}
 }
